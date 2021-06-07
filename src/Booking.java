09e88fc4ae17a64c62dd5d985f311e1fcd902145
Mohamed <mohamedelhagery@gmail.com>
2021-06-07 19:24:15 +0200

    added booking class and changed startDate type from String to Date

--- src/Booking.java
+++ src/Booking.java
@@ -1,4 +1,22 @@
-public class Booking
-{
+import java.util.Date;
+
+public class Booking {
+    Date startTime;
+    int playersCount;
+    int forPlayground; // id of the playground that accepted the booking
+    int bookingCreator; // id of the player who created the booking
+
+    public Booking(Date startTime, int playersCount, int forPlayground, int bookingCreator) {
+        this.startTime = startTime;
+        this.playersCount = playersCount;
+        this.forPlayground = forPlayground;
+        this.bookingCreator = bookingCreator;
+    }
+
+    public void print() {
+        System.out.println("The booking starts at" + startTime);
+        System.out.println("Player " + bookingCreator + " has booked the playground " + forPlayground + " for " +
+                playersCount + " people.");
+    }
 
 }
